e47555183deb7ca2db0ba7524a25a8987b73c3b3
david <jdchavezr917@gmail.com>
2026-04-27 10:20:42 -0600

    penultimos toques

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss({
+      // Configuración para evitar el error
+      apply: 'build',
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "tailwindcss";`
+      }
+    }
+  }
 })
